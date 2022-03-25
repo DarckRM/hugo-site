470d51347c91a98f8f30f6588f83cfda1bf01112
DarckRM <412543224@qq.com>
2022-03-25 17:30:35 +0800

    周五 2022/03/25 17:30:35.48 代码更新

--- 编译并部署至服务器.bat
+++ 编译并部署至服务器.bat
@@ -1,6 +1,14 @@
 @echo off
 chcp 65001
-echo 开始编译
+echo 检查原始代码更新
+git add .
+git commit -m "%date% %time% 代码更新"
+echo 拉取远程代码
+git pull
+echo 上传远程代码
+git push
+echo 已完成原始代码更新，请按任意键编译并部署至服务器
+pause
 .\hugo -D
 cd public
 git add .
