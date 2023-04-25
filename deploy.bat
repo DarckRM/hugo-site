eea41c4b5cfd47ced819be66adb099d36c50dbd7
DarckRM <412543224@qq.com>
2023-04-26 07:12:42 +0800

    update src file 周三 2023/04/26  7:12:42.60

--- deploy.bat
+++ deploy.bat
@@ -1,18 +1,17 @@
 @echo off
 chcp 65001
 echo 检查原始代码更新
-git add .
-git commit -m "%date% %time% 代码更新"
-echo 拉取远程代码
 git pull
+git add .
+git commit -m "update src file %date% %time%"
 echo 上传远程代码
 git push
 echo 已完成原始代码更新，请按任意键编译并部署至服务器
 pause
 .\hugo -D
 cd public
+git pull
 git add .
 git commit -m "%date% %time% 更新内容"
-git pull
 git push
 pause
